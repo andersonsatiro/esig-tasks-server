3f2d9ba9c88a2109b42a4d8cb4b90da615c7cd88
AndersonPaivaS <andersonpaiva135@gmail.com>
2024-08-05 18:04:21 -0300

    feat: create 'get all tasks' endpoint

---
+++ prisma/migrations/20240805204316_update_task_creation_date_column/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Task" ALTER COLUMN "creation_date" DROP DEFAULT;
--- src/routes/GET/get-all-tasks.ts
+++ src/routes/GET/get-all-tasks.ts
@@ -1,11 +1,17 @@
-import { FastifyInstance } from 'fastify';
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import { prisma } from "../../lib/prisma";
 
 export async function GetAllTasks(app: FastifyInstance) {
-  app.get('/tasks', async (request, reply) => {
-    const tasks = [
-      { id: 1, description: 'Learn Fastify', status: 'pending' },
-      { id: 2, description: 'Set up TypeScript', status: 'completed' },
-    ];
-    reply.send({ tasks });
+  app.get('/tasks', async (req: FastifyRequest, reply: FastifyReply) => {
+    try {
+      const tasks = await prisma.task.findMany();
+      reply.code(200).send(tasks)
+    } catch(error){
+      console.error(error)
+      reply.code(500).send({
+        status: 'erro',
+        message: 'Não foi possível buscar as tarefas. Tente novamente mais tarde.'
+      })
+    }
   });
 }
